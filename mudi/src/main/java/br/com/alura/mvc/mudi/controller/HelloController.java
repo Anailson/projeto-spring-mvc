c64bee59937d2fdf6d3781e3178936c15e482e15
anailson <arsantos315@gmail.com>
2023-01-30 08:26:41 -0300

    criando o modelo

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -1,18 +1,16 @@
 package br.com.alura.mvc.mudi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
-	
-	
+
 	@GetMapping("/hello")
-	public String hello(HttpServletRequest request) {
-		request.setAttribute("nome", "Mundo");
-			
+	public String hello(Model model) {
+		model.addAttribute("nome", "Mundo");
+
 		return "hello";
 	}
 
